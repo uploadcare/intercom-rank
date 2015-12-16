2d3075a5421c929e7ee169f360ab2c03520f3f82
Dmitry Nazarov <mail@mkn8rd.org>
2015-12-16 16:59:31 +0300

    ..

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'  # todo
 
-# todo switch to PostgreSQL later
 SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/intercom-rank_dev'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
