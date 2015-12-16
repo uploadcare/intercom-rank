2b7e2a9408ce4fc076741f497abb38a4d18400ab
Dmitry Nazarov <mail@mkn8rd.org>
2015-12-16 18:36:47 +0300

    typo fxd: DATABASE_URL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'  # todo env
 
-if os.environ.get('DATABSE_URL') is None:
+if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/intercom-rank_dev'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
