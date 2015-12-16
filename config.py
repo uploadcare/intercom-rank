4fc761a8b7b36c98093f81b2bed6ac6c0740a005
Dmitry Nazarov <mail@mkn8rd.org>
2015-12-16 18:31:39 +0300

    SQLALCHEMY_DATABASE_URI = os.environ[DATABASE_URL]

--- config.py
+++ config.py
@@ -3,9 +3,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'  # todo
+SECRET_KEY = 'you-will-never-guess'  # todo env
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/intercom-rank_dev'
+if os.environ.get('DATABSE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/intercom-rank_dev'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 DEBUG = True
